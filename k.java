bc3371e852d02dc90a054aff316f27a881f606e4
Katadiraju <101514750+Katadiraju@users.noreply.github.com>
2022-12-02 23:29:01 +0530

    Add files via upload

---
+++ k.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class k
+{
+  static Scanner obj=new Scanner(System.in);
+  String m1(int a)
+  {
+    System.out.print(a);
+    return obj.next();
+  }
+  class A 
+  {
+     String m2(int b)
+     {
+       class 
+       System.out.print(b);
+       return obj.next();
+     }
+  }
+  class b
+  {
+      String m3(int c)
+      {
+        System.out.print(c);
+        return obj.next();
+
+      }
+  }
+  public static void main(String[] args) {
+    System.out.print("hi ammakjdikecl");
+  }
+}
---
+++ raju.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+class s
+{
+  public static void main(String[] args) {
+    System.out.print("hi amma");
+  }
+}
